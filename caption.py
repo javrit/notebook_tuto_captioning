3516af1489e233f6a92c5635b1981a93856adc17
javrit <julienavrit@hotmail.fr>
2025-02-03 05:25:25 +1100

    Update caption.py

--- caption.py
+++ caption.py
@@ -3,15 +3,71 @@ import torch.nn.functional as F
 import numpy as np
 import json
 import torchvision.transforms as transforms
+
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import skimage.transform
 import argparse
-from scipy.misc import imread, imresize
+import imageio.v2 as imageio
+from PIL import Image
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# Import your classes (adjust the import if your classes are defined in a module)
+# Add these classes to the safe globals list.
+# Now load the checkpoint with weights_only=True.
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import torch
+import torch.nn.functional as F
+import numpy as np
+import json
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import skimage.transform
+import argparse
+import imageio  # replacing scipy.misc.imread
 from PIL import Image
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Import your classes from models.py
+from models import Encoder, Attention, DecoderWithAttention
+torch.serialization.add_safe_globals([Encoder, DecoderWithAttention, Attention])
+
+# PATCH: Monkey-patch torch.optim.Adam.__setstate__ to add a default if missing.
+import torch.optim
+def patched_adam_setstate(self, state):
+    if 'defaults' not in state:
+        state['defaults'] = {'differentiable': False}
+    super(torch.optim.Adam, self).__setstate__(state)
+torch.optim.Adam.__setstate__ = patched_adam_setstate
+
+# Now, parse arguments, etc.
+parser = argparse.ArgumentParser(description='Show, Attend, and Tell - Tutorial - Generate Caption')
+parser.add_argument('--img', '-i', help='path to image')
+parser.add_argument('--model', '-m', help='path to model')
+parser.add_argument('--word_map', '-wm', help='path to word map JSON')
+parser.add_argument('--beam_size', '-b', default=5, type=int, help='beam size for beam search')
+parser.add_argument('--dont_smooth', dest='smooth', action='store_false', help='do not smooth alpha overlay')
+args = parser.parse_args()
+
+# Load checkpoint with full state
+checkpoint = torch.load(args.model, map_location=str(device))
+if 'optimizer' in checkpoint:
+    del checkpoint['optimizer']
+decoder = checkpoint['decoder']
+decoder = decoder.to(device)
+decoder.eval()
+encoder = checkpoint['encoder']
+encoder = encoder.to(device)
+encoder.eval()
+
+# Load word map (word2ix)
+with open(args.word_map, 'r') as j:
+    word_map = json.load(j)
+rev_word_map = {v: k for k, v in word_map.items()}  # ix2word
 
 def caption_image_beam_search(encoder, decoder, image_path, word_map, beam_size=3):
     """
@@ -20,65 +76,65 @@ def caption_image_beam_search(encoder, decoder, image_path, word_map, beam_size=
     :param encoder: encoder model
     :param decoder: decoder model
     :param image_path: path to image
-    :param word_map: word map
+    :param word_map: word map (dictionary mapping words to indices)
     :param beam_size: number of sequences to consider at each decode-step
-    :return: caption, weights for visualization
+    :return: caption sequence (list of indices), weights for visualization
     """
 
     k = beam_size
     vocab_size = len(word_map)
 
     # Read image and process
-    img = imread(image_path)
+    img = imageio.imread(image_path)
     if len(img.shape) == 2:
-        img = img[:, :, np.newaxis]
-        img = np.concatenate([img, img, img], axis=2)
-    img = imresize(img, (256, 256))
-    img = img.transpose(2, 0, 1)
-    img = img / 255.
+        # If grayscale, stack to get three channels
+        img = np.stack([img]*3, axis=-1)
+    # Resize using skimage.transform.resize; it returns a float array in [0,1]
+    img = skimage.transform.resize(img, (256, 256), anti_aliasing=True)
+    img = img.transpose(2, 0, 1)  # (3, 256, 256)
+    # No need to divide by 255 because resize already normalizes to [0,1]
     img = torch.FloatTensor(img).to(device)
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
     transform = transforms.Compose([normalize])
     image = transform(img)  # (3, 256, 256)
 
-    # Encode
+    # Encode the image
     image = image.unsqueeze(0)  # (1, 3, 256, 256)
     encoder_out = encoder(image)  # (1, enc_image_size, enc_image_size, encoder_dim)
     enc_image_size = encoder_out.size(1)
     encoder_dim = encoder_out.size(3)
 
-    # Flatten encoding
-    encoder_out = encoder_out.view(1, -1, encoder_dim)  # (1, num_pixels, encoder_dim)
+    # Flatten encoding: (1, num_pixels, encoder_dim)
+    encoder_out = encoder_out.view(1, -1, encoder_dim)
     num_pixels = encoder_out.size(1)
 
-    # We'll treat the problem as having a batch size of k
+    # Expand to have a batch size equal to beam size k
     encoder_out = encoder_out.expand(k, num_pixels, encoder_dim)  # (k, num_pixels, encoder_dim)
 
-    # Tensor to store top k previous words at each step; now they're just <start>
+    # Tensor to store top k previous words; start with <start>
     k_prev_words = torch.LongTensor([[word_map['<start>']]] * k).to(device)  # (k, 1)
 
-    # Tensor to store top k sequences; now they're just <start>
+    # Tensor to store top k sequences; initially just <start>
     seqs = k_prev_words  # (k, 1)
 
-    # Tensor to store top k sequences' scores; now they're just 0
+    # Tensor to store top k sequences' scores; initially zeros
     top_k_scores = torch.zeros(k, 1).to(device)  # (k, 1)
 
-    # Tensor to store top k sequences' alphas; now they're just 1s
+    # Tensor to store top k sequences' alphas; initially ones
     seqs_alpha = torch.ones(k, 1, enc_image_size, enc_image_size).to(device)  # (k, 1, enc_image_size, enc_image_size)
 
     # Lists to store completed sequences, their alphas and scores
-    complete_seqs = list()
-    complete_seqs_alpha = list()
-    complete_seqs_scores = list()
+    complete_seqs = []
+    complete_seqs_alpha = []
+    complete_seqs_scores = []
 
     # Start decoding
     step = 1
     h, c = decoder.init_hidden_state(encoder_out)
 
-    # s is a number less than or equal to k, because sequences are removed from this process once they hit <end>
+    # s is the number of active beams (<= k)
     while True:
-
         embeddings = decoder.embedding(k_prev_words).squeeze(1)  # (s, embed_dim)
 
         awe, alpha = decoder.attention(encoder_out, h)  # (s, encoder_dim), (s, num_pixels)
@@ -93,34 +149,33 @@ def caption_image_beam_search(encoder, decoder, image_path, word_map, beam_size=
         scores = decoder.fc(h)  # (s, vocab_size)
         scores = F.log_softmax(scores, dim=1)
 
-        # Add
+        # Add cumulative scores
         scores = top_k_scores.expand_as(scores) + scores  # (s, vocab_size)
 
-        # For the first step, all k points will have the same scores (since same k previous words, h, c)
+        # For the first step, all k beams have the same scores (only one <start> token)
         if step == 1:
-            top_k_scores, top_k_words = scores[0].topk(k, 0, True, True)  # (s)
+            top_k_scores, top_k_words = scores[0].topk(k, 0, True, True)  # (k)
         else:
-            # Unroll and find top scores, and their unrolled indices
-            top_k_scores, top_k_words = scores.view(-1).topk(k, 0, True, True)  # (s)
+            # Unroll and find top scores and their indices
+            top_k_scores, top_k_words = scores.view(-1).topk(k, 0, True, True)  # (k)
 
-        # Convert unrolled indices to actual indices of scores
-        prev_word_inds = top_k_words / vocab_size  # (s)
-        next_word_inds = top_k_words % vocab_size  # (s)
+        # Convert unrolled indices to actual indices: prev beam index and next word index
+        prev_word_inds = top_k_words // vocab_size  # (k)
+        next_word_inds = top_k_words % vocab_size   # (k)
 
-        # Add new words to sequences, alphas
-        seqs = torch.cat([seqs[prev_word_inds], next_word_inds.unsqueeze(1)], dim=1)  # (s, step+1)
+        # Add new words to sequences and alphas
+        seqs = torch.cat([seqs[prev_word_inds], next_word_inds.unsqueeze(1)], dim=1)  # (k, step+1)
         seqs_alpha = torch.cat([seqs_alpha[prev_word_inds], alpha[prev_word_inds].unsqueeze(1)],
-                               dim=1)  # (s, step+1, enc_image_size, enc_image_size)
+                               dim=1)  # (k, step+1, enc_image_size, enc_image_size)
 
-        # Which sequences are incomplete (didn't reach <end>)?
-        incomplete_inds = [ind for ind, next_word in enumerate(next_word_inds) if
-                           next_word != word_map['<end>']]
+        # Check which sequences have not reached <end>
+        incomplete_inds = [ind for ind, next_word in enumerate(next_word_inds) if next_word != word_map['<end>']]
         complete_inds = list(set(range(len(next_word_inds))) - set(incomplete_inds))
 
         # Set aside complete sequences
         if len(complete_inds) > 0:
-            complete_seqs.extend(seqs[complete_inds].tolist())
-            complete_seqs_alpha.extend(seqs_alpha[complete_inds].tolist())
+            complete_seqs.extend([seqs[ind].tolist() for ind in complete_inds])
+            complete_seqs_alpha.extend([seqs_alpha[ind].tolist() for ind in complete_inds])
             complete_seqs_scores.extend(top_k_scores[complete_inds])
         k -= len(complete_inds)  # reduce beam length accordingly
 
@@ -135,11 +190,15 @@ def caption_image_beam_search(encoder, decoder, image_path, word_map, beam_size=
         top_k_scores = top_k_scores[incomplete_inds].unsqueeze(1)
         k_prev_words = next_word_inds[incomplete_inds].unsqueeze(1)
 
-        # Break if things have been going on too long
         if step > 50:
             break
         step += 1
 
+    if len(complete_seqs) == 0:
+        complete_seqs = [seq.tolist() for seq in seqs]
+        complete_seqs_scores = top_k_scores.squeeze(1).tolist()
+
+    # Choose the best beam (highest cumulative log-probability)
     i = complete_seqs_scores.index(max(complete_seqs_scores))
     seq = complete_seqs[i]
     alphas = complete_seqs_alpha[i]
@@ -151,38 +210,44 @@ def visualize_att(image_path, seq, alphas, rev_word_map, smooth=True):
     """
     Visualizes caption with weights at every word.
 
-    Adapted from paper authors' repo: https://github.com/kelvinxu/arctic-captions/blob/master/alpha_visualization.ipynb
+    Adapted from the paper authors' repo.
 
-    :param image_path: path to image that has been captioned
-    :param seq: caption
-    :param alphas: weights
-    :param rev_word_map: reverse word mapping, i.e. ix2word
-    :param smooth: smooth weights?
+    :param image_path: path to the image that has been captioned
+    :param seq: caption (list of indices)
+    :param alphas: attention weights
+    :param rev_word_map: reverse word mapping, i.e., index-to-word dictionary
+    :param smooth: whether to smooth the weights
     """
     image = Image.open(image_path)
-    image = image.resize([14 * 24, 14 * 24], Image.LANCZOS)
+    plt.imshow(image)
+
+    image = image.resize((14 * 24, 14 * 24), Image.LANCZOS)
+    plt.imshow(image)
 
     words = [rev_word_map[ind] for ind in seq]
 
+    n_rows = int(np.ceil(len(words) / 5.0))
     for t in range(len(words)):
         if t > 50:
             break
-        plt.subplot(np.ceil(len(words) / 5.), 5, t + 1)
-
+        plt.subplot(n_rows, 5, t + 1)
         plt.text(0, 1, '%s' % (words[t]), color='black', backgroundcolor='white', fontsize=12)
         plt.imshow(image)
+
         current_alpha = alphas[t, :]
         if smooth:
-            alpha = skimage.transform.pyramid_expand(current_alpha.numpy(), upscale=24, sigma=8)
+            alpha = skimage.transform.pyramid_expand(current_alpha, upscale=24, sigma=8)
         else:
-            alpha = skimage.transform.resize(current_alpha.numpy(), [14 * 24, 14 * 24])
+            alpha = skimage.transform.resize(current_alpha, (14 * 24, 14 * 24))
         if t == 0:
             plt.imshow(alpha, alpha=0)
         else:
             plt.imshow(alpha, alpha=0.8)
         plt.set_cmap(cm.Greys_r)
         plt.axis('off')
+    plt.savefig('caption.png')
     plt.show()
+
 
 
 if __name__ == '__main__':
@@ -198,6 +263,8 @@ if __name__ == '__main__':
 
     # Load model
     checkpoint = torch.load(args.model, map_location=str(device))
+    if 'optimizer' in checkpoint:
+      del checkpoint['optimizer']
     decoder = checkpoint['decoder']
     decoder = decoder.to(device)
     decoder.eval()
